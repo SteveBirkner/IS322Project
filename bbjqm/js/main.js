f98dd2eecd96c0839c67d331e9d2ea6e2b67ada7
matthew toegel dd student <mt85@afsconnect2.njit.edu>
2013-04-08 15:13:11 -0400

    Integated search

--- bbjqm/js/main.js
+++ bbjqm/js/main.js
@@ -80,7 +80,15 @@ $(document).ready(function () {
     app = new AppRouter();
     Backbone.history.start();
 });
-
+$(document).keypress(function(e) {
+        if(e.which == 13) {
+            var s = $("#search").val();
+            if(s.length > 0){
+                console.log("clicked");
+                search(s);
+            }
+        }
+    });
 function search(str){
     var apikey = "d9cbk342np3k8jj9ntmybz5f";
     var url = "http://api.remix.bestbuy.com/v1/products(search=" + escape(str) + ")?apiKey=" + apikey + "&show=name,sku,regularPrice&sort=regularPrice.asc&format=json";
